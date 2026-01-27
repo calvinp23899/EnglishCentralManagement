a9cc43895c1959a52f846fde117452156192949f
calvin <calvinp2381999@gmail.com>
2026-01-27 20:57:51 +0700

    build profile basic ui

---
+++ Areas/Admin/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishCentralManagement.Areas.Admin.Controllers
+{
+    public class ProfileController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
