9fedef64836ee146d9a8374eb083eae7df82d2e9
calvin <calvinp2381999@gmail.com>
2026-02-10 20:29:57 +0700

    crud course

---
+++ Dtos/CourseDto.cs
@@ -0,0 +1,20 @@
+﻿namespace EnglishCentralManagement.Dtos
+{
+    public class CourseDto
+    {
+        public long Id { get; set; }
+        public string CourseName { get; set; }
+        public int DurationInMonth { get; set; }
+        public decimal? MonthlyFee { get; set; }
+        public decimal? Total
+        {
+            get
+            {
+                var total = DurationInMonth * MonthlyFee;
+                return total;
+            }
+        }
+        public string? CreatedBy { get; set; }
+        public DateTimeOffset? CreatedDate { get; set; }
+    }
+}
